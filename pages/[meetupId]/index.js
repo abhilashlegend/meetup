df7f26982e7c06bb4a826482eca77c63f5210b14
abhilashlegend <abhilash456a@live.com>
2026-01-22 13:02:51 +0530

    Added dynamic value to meetup detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,37 @@
-export default function MeetupDetailsPage() {
-    return <h1>Meetup Details Page</h1>
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+export default function MeetupDetailsPage(props) {
+    return <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+    />
+}
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+
+    return {
+        props: {
+            meetupData: {   
+
+                id: meetupId,
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
+                title: 'A First Meetup',    
+                address: 'Some address 5, 12345 Some City',
+                description: 'This is a first meetup!'
+            }
+        }
+    }
+}   
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { meetupId: 'm1' } },
+            { params: { meetupId: 'm2' } }
+        ],
+        fallback: false
+    }
 }
