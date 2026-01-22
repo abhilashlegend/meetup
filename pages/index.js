e7e4dae2efd882f2bd9abe8d419aad541618961a
abhilashlegend <abhilash456a@live.com>
2026-01-22 18:51:50 +0530

    Added meetups from mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList"
-
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a first meetup!'
-    },
-    {
-        id: 'm2',
-        title: 'A Second Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 10, 12345 Some City',
-        description: 'This is a second meetup!'
-    }
-];
+import { MongoClient } from "mongodb";
 
 
 export default function HomePage(props) {
@@ -26,9 +10,22 @@ export default function HomePage(props) {
 }
 
 export async function getStaticProps() {
+
+    const client = await MongoClient.connect('mongodb://localhost:27017/meetups');
+
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+    const meetups = await meetupsCollection.find().toArray();
+    client.close();
+
     return {
         props: {
-            meetups: DUMMY_MEETUPS
+            meetups: meetups.map(meetup => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString()
+            }))
         },
         revalidate: 1
     }
